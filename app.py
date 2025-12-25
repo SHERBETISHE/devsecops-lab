a337d377ceb40401bc5a12fc75ea4974dd9cd471
administrator <buxoidima@gmail.com>
2025-12-25 18:20:41 +0000

    fix

--- app.py
+++ app.py
@@ -7,4 +7,5 @@ def health():
     return {"status": "ok"}, 200
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    # Мы используем nosemgrep, так как 0.0.0.0 необходим для работы внутри Docker контейнера
+    app.run(host="0.0.0.0", port=8080) # nosemgrep: python.flask.security.audit.app-run-param-config.avoid_app_run_with_bad_host
